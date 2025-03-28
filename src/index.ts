8b1a8ccfd5a17be83ff8373cb19964aeaff3bd41
Hansen <hansenreizo@gmail.com>
2025-03-28 14:32:24 +1000

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 console.log("Hello Task Management System!");
+console.log("Welcome to the Task Management System!");
+console.log("This is a simple task management system built with TypeScript.");  
